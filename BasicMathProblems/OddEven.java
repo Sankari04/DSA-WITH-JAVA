cc339549fc9cec17e53a1768ba679a0917c21728
Sankari04 <n.sankari2004@gmail.com>
2026-05-10 16:06:36 +0530

    Added OddEven basic math problem

---
+++ BasicMathProblems/OddEven.java
@@ -0,0 +1,19 @@
+class Solution{
+    boolean isEven(int N){
+        if(N%2==0){
+            return true;
+        }
+        return false;
+
+    }
+}
+
+
+
+public class OddEven{
+    public static void main(String[] args) {
+        Solution obj1=new Solution();
+        boolean res=obj1.isEven(15);
+        System.out.println(res);
+    }
+}
