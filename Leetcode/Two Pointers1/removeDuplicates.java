62ac31f9329bea7e7adac77cc6990c7d0cc39eee
Sankari04 <n.sankari2004@gmail.com>
2026-05-25 22:23:32 +0530

    Solved Rremove Duplicates

---
+++ Leetcode/Two Pointers1/removeDuplicates.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int i = 0;
+
+        for(int k = 1; k < nums.length; k++) {
+            if(nums[i] != nums[k]) {
+                i++;
+                nums[i] = nums[k];
+            }
+        }
+
+        return i + 1;
+    }
+}
